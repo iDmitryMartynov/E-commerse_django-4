4624511905dc331f6832a61700f38933a97726d3
Dmitry <dmitry1298@yandex.ru>
2024-08-11 17:30:54 +1000

    12

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from myapp.views import index, indexItem, add_item
+from myapp.views import index, indexItem, add_item, update_item
 
 app_name = 'myapp'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:my_id>/', indexItem, name='detail'),
     #http://127.0.0.1:8000/myapp/
     path('additem/', add_item, name='add_item'),
+    path('updateitem/<int:my_id>/', update_item, name='update_item'),
 ]
