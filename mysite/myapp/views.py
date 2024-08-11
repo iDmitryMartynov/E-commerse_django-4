2e2eee54802312aa494522b4ae5aa503c64162be
Dmitry <dmitry1298@yandex.ru>
2024-08-11 23:19:57 +1000

    13

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -37,3 +37,12 @@ def update_item(request, my_id):
         return redirect("/myapp/")
     context = {"item": item}
     return render(request, "myapp/updateitem.html", context=context)
+
+
+def delete_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/myapp/")
+    context = {"item": item}
+    return render(request, "myapp/deleteitem.html", context=context)
