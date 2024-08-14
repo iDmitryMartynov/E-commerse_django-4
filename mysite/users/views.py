19a56c79ec3d8f2d28ee6912db7963a5af4c1bf2
Dmitry <dmitry1298@yandex.ru>
2024-08-14 22:32:53 +1000

    changed functions to classes

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import NewUserForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
@@ -16,4 +18,17 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        contact_number = request.POST.get('number')
+        image = request.FILES("upload")
+        user = request.user
+        profile = Profile(user=user, contact_number=contact_number, image=image)
+        profile.save()
     return render(request, 'users/profile.html')
+
+def seller_profile(request, id):
+    seller = User.objects.get(id=id)
+
+    context = {'seller':seller}
+    
+    return render(request, 'users/sellerprofile.html', context)
