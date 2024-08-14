b4bdb6b2cbf88b118765b4419f680776cd4a1c18
Dmitry <dmitry1298@yandex.ru>
2024-08-14 16:38:10 +1000

    add info about every user

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -15,13 +16,15 @@ def indexItem(request, my_id):
     return render(request, "myapp/detail.html", context=context)
 
 
+@login_required
 def add_item(request):
     if request.method == "POST":
         name = request.POST.get("name")
         price = request.POST.get("price")
         description = request.POST.get("description")
         image = request.FILES["upload"]
-        item = Product(name=name, price=price, description=description, image=image)
+        seller = request.user
+        item = Product(name=name, price=price, description=description, image=image, seller=seller)
         item.save()
     return render(request, "myapp/additem.html")
 
